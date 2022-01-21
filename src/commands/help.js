76af697dfa6406bcd408aacc64eba8ea36a2ce0d
legendnightt <alex@legend.sh>
2022-01-21 14:24:10 +0100

    new: Administrator commands support

--- src/commands/help.js
+++ src/commands/help.js
@@ -5,20 +5,32 @@ module.exports = {
     description: 'command help',
     run(message, commands) {
         const embed = {
-            color: config.colors.orange,
-            author: { name: 'Commands Help' },
+            color: config.colors.blue,
+            thumbnail: { url: message.guild.members.cache.get(config.clientid).displayAvatarURL({ size: 4096, dynamic: true }) },
             fields: [],
             timestamp: new Date(),
             footer: { text: `ID: ${message.member.id}` }
-        };
+        }
+        // Type of help is going to display
+        if (message.member.permissions.has(['ADMINISTRATOR'] || [])) {
+            embed.author = { name: 'Administrator Commands Help' };
+        } else {
+            embed.author = { name: 'Commands Help' };
+        }
         // commands fields info
         var cont = 0;
         for (let [name] of commands) {
             if (commands.get(name).name != 'help') {
-                embed.fields[cont] = { name: `.${commands.get(name).name}`, value: commands.get(name).description };
+                if (commands.get(name).permission != null) {
+                    if (message.member.permissions.has(commands.get(name).permission || [])) {
+                        embed.fields[cont] = { name: `.${commands.get(name).name}`, value: commands.get(name).description, inline: true };
+                    }
+                } else {
+                    embed.fields[cont] = { name: `.${commands.get(name).name}`, value: commands.get(name).description, inline: true };
+                }
                 cont++;
-            };
-        };
+            }
+        }
         message.reply({ embeds: [embed] });
     }
-};
+}
--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,5 +1,13 @@
 const config = require('../config/config.json');
 
+function runCommand(commands, command, message, args) {
+    if (command.name === 'help') {
+        command.run(message, commands);
+    } else {
+        command.run(message, args);
+    }
+}
+
 module.exports = {
     name: 'messageCreate',
     once: false,
@@ -10,17 +18,21 @@ module.exports = {
         let messageArray = message.content.split(" ");
         let cmd = messageArray[0];
         let args = messageArray.slice(1);
-        let command = commands.get(cmd.slice(config.prefix.length))
+        let command = commands.get(cmd.slice(config.prefix.length));
         if(command) {
             try {
-                if (command.name === 'help')
-                    command.run(message, commands);
-                else
-                    command.run(message, args);
+                if (command.permission != null) {
+                    if (message.member.permissions.has(command.permission || [])) {
+                        runCommand(commands, command, message, args);
+                    } else {
+                        message.reply(`You don't have permission to execute this command!`);
+                    }
+                } else {
+                    runCommand(commands, command, message, args);
+                }
             } catch(e) {
-                console.log(`[ERROR] Command ${command} not found`);
                 console.log(e.stack);
-            };
-        };
+            }
+        }
     }
-};
+}
