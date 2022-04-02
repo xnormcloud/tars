5c2ebebe6bc1768855b83fcc067aff23dae0ff05
legendnightt <alex@legend.sh>
2022-04-02 02:42:09 +0200

    feat: basic tickets validator

--- src/utils/ticket.js
+++ src/utils/ticket.js
@@ -1,22 +1,21 @@
 const config = require('../config/config.json');
 
 module.exports.create = (type, customer, message) => {
-    const category = config.categories.map(group => group === type);
-    console.log(category);
+    const category = config.categories.filter(group => group.name === type)[0];
     if (category !== undefined) {
         if (!exists(category, customer, message)) {
-            message.guild.channels.create(customer).then(channel => channel.setParent(category));
-            message.reply('Channel successfully created!');
+            message.guild.channels.create(customer).then(channel => channel.setParent(category.id));
+            message.reply(`Channel successfully created in \`${category.name}\` category!`);
         }
         else {
-            message.reply('Already existing channel!');
+            message.reply(`Already existing channel in \`${category.name}\` category!`);
         }
     }
     else {
-        message.reply('That category does not exists');
+        message.reply(`\`${type}\` category doesn't exists!`);
     }
 };
 
 function exists(category, customer, message) {
-    return message.guild.channels.cache.filter(channel => channel.name === customer && channel.parentId === category).size !== 0;
+    return message.guild.channels.cache.filter(channel => channel.name === customer && channel.parentId === category.id).size !== 0;
 }
