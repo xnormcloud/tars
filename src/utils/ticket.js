419ea65a49a93752cd6055b099482403a1b9745a
legendnightt <alex@legend.sh>
2022-04-02 03:38:43 +0200

    feat: tickets welcome embed

--- src/utils/ticket.js
+++ src/utils/ticket.js
@@ -1,10 +1,38 @@
+const { MessageActionRow, MessageButton } = require('discord.js');
 const config = require('../config/config.json');
 
 module.exports.create = (type, customer, message) => {
     const category = config.categories.filter(group => group.name === type)[0];
     if (category !== undefined) {
         if (!exists(category, customer, message)) {
-            message.guild.channels.create(customer).then(channel => channel.setParent(category.id));
+            message.guild.channels.create(customer).then(channel => {
+                channel.setParent(category.id).then(() => {
+                    const embed = {
+                        color: config.colors.blue,
+                        author: { name: `Welcome to ${category.name} ticket` },
+                        description: 'Please be patient, we will answer as soon as possible',
+                        fields: [ { name: 'Important info', value: category.info.join('') } ],
+                        timestamp: new Date(),
+                        footer: { text: `ID: ${channel.id}` },
+                    };
+                    const buttons = new MessageActionRow()
+                        .addComponents(
+                            new MessageButton()
+                                .setCustomId('close')
+                                .setLabel('Close')
+                                .setStyle('SUCCESS'),
+                            new MessageButton()
+                                .setCustomId('delete')
+                                .setLabel('Delete')
+                                .setStyle('DANGER'),
+                            new MessageButton()
+                                .setURL('https://xnorm.cloud')
+                                .setLabel('Website')
+                                .setStyle('LINK'),
+                        );
+                    channel.send({ embeds: [embed], components: [buttons] });
+                });
+            });
             message.reply(`Channel successfully created in \`${category.name}\` category!`);
         }
         else {
